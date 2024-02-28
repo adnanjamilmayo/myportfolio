48122e5fcd0d6838e1947c8a4014d4051a1adad5
Adnan <adnanjamilrajpoot@gmail.com>
2024-02-29 02:27:47 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+
+# AJFolio
+
+AJFolio is my own personal portfolio built using React.js and Tailwind CSS. It offers a simple means for developers to exhibit their work, expertise, and accomplishments in a professional and visually engaging manner. DSFolio stands out with its adaptable design and well-organized code, allowing effortless customization to meet the unique requirements and tastes of users. This makes it a perfect option for individuals seeking to craft an impressive online portfolio.
+
+<center>
+    <img src="/public/cover.jpeg" alt="Reactfolio" />
+</center>
+
+<center>
+
+<!-- 
+<img  src="https://api.netlify.com/api/v1/badges/17df56e3-143c-484d-a3c3-e1fb35a734dd/deploy-status"  alt="Netlify Status"/> -->
+
+</center>
+
+## Technologies Used
+<img  src="https://skillicons.dev/icons?i=html,css,react,nodejs,netlify&perline=7"/>
+
+
+## Usage
+To start the development server and view the project in your browser, run the following command:
+```bash
+yarn dev
+```
+Visit http://localhost:5173/ in your browser
+
+## Configurations
+- You can change content of the website at `/data/user_info.js`
+- Change the Google Analytics MEASUREMENT ID at `index.html` to your own.
+
+## Contributing
+Feel free to contribute to this project by opening issues or submitting pull requests. Your feedback is highly appreciated!
---
+++ src/data/user_info.js
@@ -0,0 +1,76 @@
+const info = {
+    main: {
+        name: "Adnan Jamil",
+        description: "I am a full stack developer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+        role: "Full Stack Developer",
+        photo: "../photo.jpg",
+        email: "adnanjamilrajpoot@gmail.com",
+        tracking_id: "G-BNTTBGHXSC"
+    },
+
+    socials: {
+        twitter: "https://twitter.com/iadnanjamil",
+        github: "https://github.com/adnanjamilmayo",
+        linkedin: "https://www.linkedin.com/in/adnanjamilmeo/",
+        instagram: "https://instagram.com/adnan.jamill/",
+        // facebook: "",
+        // telegram: ""
+    },
+
+    skills: {
+        languages: {
+            description: "I possess proficient expertise in a diverse range of programming languages, including C, C++, TypeScript, and JavaScript.",
+            image: "https://skillicons.dev/icons?i=c,cpp,typescript,javascript&perline=6&theme="
+        },
+        frameworks: {
+            description: "Experienced in a versatile tech stack, including React, Express.js, Node.js, Tailwind CSS",
+            image: "https://skillicons.dev/icons?i=react,express,nodejs,tailwind&perline=6&theme="
+        },
+        tools: {
+            description: "I am well-versed in utilizing various tools and platforms to enhance the development process, including Git, Github, Netlify, Vercel and Postman.",
+            image: "https://skillicons.dev/icons?i=git,github,netlify,vercel,postman&perline=6&theme="
+        },
+    },
+
+    projects: [
+        {
+            title: "E-Commerce App",
+            description: "This is my final year project, In my final year project, I spearheaded the development of a full-fledged e-commerce web application using the MERN stack. This hands-on experience showcased my proficiency in full-stack web development and demonstrated my ability to deliver dynamic and user-centric digital solutions.",
+            technologies: "https://skillicons.dev/icons?i=mongodb,express,nodejs,react,tailwindcss,0,0&perline=5&theme=",
+            // github: "",
+            // link: ""
+        },
+
+        {
+            title: "MICare App",
+            description: "An offline first suite of applications designed to deliver nutritional and health products to expecting and lactating mothers. The platform manages patient profiles, sales and visit data, and generates comprehensive metrics for all involved stakeholders. Collaboratively designed, architected, and led a team to rapidly build the platform.",
+            technologies: "https://skillicons.dev/icons?i=ts,react,tailwindcss,0,0&perline=5&theme=",
+            // github: "",
+            // link: ""
+        },
+    ],
+
+    education: [
+        {
+            school: "University of Management & Technology",
+            degree: "BS in Software Engineering",
+            year: "2019 - 2023",
+            image: "umt.png"
+        }
+        // {
+        //     school: "STI College",
+        //     degree: "ICT in Mobile App and Web Development",
+        //     year: "2018 - 2020",
+        //     image: "sti.png"
+        // },
+    ],
+
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at adnanjamilrajpoot@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    footer: "© 2024 Adnan Jamil. All Rights Reserved"
+}
+
+export default info
